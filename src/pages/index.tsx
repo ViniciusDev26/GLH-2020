514b7cb486a98ecd95b21900357c721f78733430
Carlos Vinicius <viniciusdev.26@gmail.com>
2022-03-25 23:27:35 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import { Header } from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Header />
+      <h2 className="font-bold underline">Body GLH</h2>
+    </>
+  )
+}
+
+export default Home
