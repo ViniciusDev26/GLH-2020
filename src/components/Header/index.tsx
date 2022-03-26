f1cb37dc1fa0a39b869d6f6084b6313f1cdc678e
Carlos Vinicius <viniciusdev.26@gmail.com>
2022-03-25 23:37:15 -0300

    feat: create first header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,37 @@
+import Image from 'next/image'
 function Header() {
   return (
-    <h2>Header</h2>
+    <nav className="flex items-center justify-between flex-wrap bg-black p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <Image 
+          src="https://assets.website-files.com/6077c884c3e246204e5f48d8/6077cccf4baced83a073cc5e_GLH-logo-Dark.svg" 
+          alt="logo" 
+          width={100}
+          height={50}
+        />
+      </div>
+      <div className="block lg:hidden">
+        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div className="text-sm lg:flex-grow">
+          {/* <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Docs
+          </a>
+          <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Examples
+          </a>
+          <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+            Blog
+          </a> */}
+        </div>
+        <div>
+          {/* <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a> */}
+        </div>
+      </div>
+    </nav>  
   );
 }
 
